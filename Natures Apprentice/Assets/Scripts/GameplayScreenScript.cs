6fd1eb12e8d9c15e3e5321fd60e801ac4405427e
Alper Üste <alperuste@gmail.com>
2020-05-08 16:14:24 +0300

    Panels added. Main Menu Added. UI added

---
+++ Natures Apprentice/Assets/Scripts/GameplayScreenScript.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameplayScreenScript : MonoBehaviour
+{
+    [SerializeField]
+    GameObject panelTutorial;
+    [SerializeField]
+    GameObject panelSettings;
+    [SerializeField]
+    GameObject panelBerryBook;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OpenTutorialPanel()
+    {
+        panelTutorial.active = true;
+    }
+    public void CloseTutorialPanel()
+    {
+        panelTutorial.active = false;
+    }
+
+    public void OpenSettingsPanel() {
+        panelSettings.active = true;
+    }
+
+    public void CloseSettingsPanel() {
+        panelSettings.active = false;
+    }
+
+    public void OpenBerryPanel()
+    {
+        panelBerryBook.active = true;
+    }
+    public void CloseBerryPanel()
+    {
+        panelBerryBook.active = false;
+    }
+}
