01a3c9a903d1e272118746aa50357e814cf3bd56
Alper Üste <alperuste@gmail.com>
2020-05-08 13:00:22 +0300

    Player Move Fixed

---
+++ Natures Apprentice/Assets/Scripts/CameraScript.cs
@@ -0,0 +1,107 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+    [SerializeField]
+    bool moveLeft = true;
+    [SerializeField]
+    bool moveRight = true;
+
+    public Transform playerTransform;
+    [SerializeField]
+    private Transform oldPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        oldPos = this.transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (moveLeft == true && moveRight == true)
+        {
+            FollowPlayer();
+        }
+        else if (moveLeft == false && moveRight == true)
+        {
+            if (oldPos.position.x > playerTransform.position.x)
+            {
+
+            }
+            else if (oldPos.position.x <= playerTransform.position.x)
+            {
+                FollowPlayer();
+            }
+        }
+        else if (moveRight == false && moveLeft == true)
+        {
+            if (oldPos.position.x < playerTransform.position.x)
+            {
+
+            }
+            else if (oldPos.position.x >= playerTransform.position.x)
+            {
+                FollowPlayer();
+            }
+        }
+    }
+
+    void FollowPlayer()
+    {
+        Transform oldPos = this.transform;
+        transform.position = new Vector3(playerTransform.position.x, this.transform.position.y, this.transform.position.z);
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.name == "LeftPlaceHolder")
+        {
+            moveLeft = false;
+        }
+        else if (collision.gameObject.name == "RightPlaceHolder")
+        {
+            moveRight = false;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.name == "LeftPlaceHolder")
+        {
+            moveLeft = true;
+        }
+        else if (collision.gameObject.name == "RightPlaceHolder")
+        {
+            moveRight = true;
+        }
+    }
+
+    //private void OnCollisionEnter2D(Collision2D collision)
+    //{
+    //    if (collision.gameObject.name == "LeftPlaceHolder")
+    //    {
+    //        moveLeft = false;
+    //    }
+    //    else if (collision.gameObject.name == "RightPlaceHolder")
+    //    {
+    //        moveRight = false;
+    //    }
+    //}
+
+    //private void OnCollisionExit2D(Collision2D collision)
+    //{
+    //    if (collision.gameObject.name == "LeftPlaceHolder")
+    //    {
+    //        moveLeft = true;
+    //    }
+    //    else if (collision.gameObject.name == "RightPlaceHolder")
+    //    {
+    //        moveRight = true;
+    //    }
+    //}
+}
---
+++ Natures Apprentice/Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    Rigidbody2D rb;
+    SpriteRenderer characterSprite;
+
+    [SerializeField]
+    bool moveLeft = true;
+    [SerializeField]
+    bool moveRight = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = this.GetComponent<Rigidbody2D>();
+        characterSprite = this.GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    void Move()
+
+    {
+
+        if (Input.GetKey(KeyCode.A) && moveLeft == true)
+        {
+            characterSprite.flipX = true;
+            this.transform.position = new Vector2(this.transform.position.x - 0.05f, this.transform.position.y);
+            //rb.AddForce(Vector2.left * 2);
+        }
+        if (Input.GetKey(KeyCode.D) && moveRight == true)
+        {
+            //rb.AddForce(Vector2.right * Time.deltaTime);
+            characterSprite.flipX = false;
+            this.transform.position = new Vector2(this.transform.position.x + 0.05f, this.transform.position.y);
+
+            //rb.AddForce(Vector2.right * 2);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.name == "LeftPlaceHolder")
+        {
+            moveLeft = false;
+        }
+        else if (collision.gameObject.name == "RightPlaceHolder")
+        {
+            moveRight = false;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.name == "LeftPlaceHolder")
+        {
+            moveLeft = true;
+        }
+        else if (collision.gameObject.name == "RightPlaceHolder")
+        {
+            moveRight = true;
+        }
+    }
+}
